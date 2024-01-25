87f6599663bdf14b49fa5a84cd296d1b8f610881
Parker Lauders <parkerlauders04@gmail.com>
2024-01-25 14:54:31 -0500

    Completed 'JavaScript' assignment

---
+++ Javascript/src/main.py
@@ -0,0 +1,34 @@
+import asyncio
+import os, os.path
+import tornado.web
+
+## Set the path to the HTML directory
+HTMLDIR = os.path.abspath(
+    os.path.join(os.path.dirname(__file__),
+                 "..","html")
+)
+
+## Function to make the application
+def MakeApp():
+    ## Points to the Profile class handler
+    endpoints = [
+        
+    ]
+
+    ## Construct application
+    app = tornado.web.Application(endpoints,
+        static_path = HTMLDIR
+    )
+
+    ## Make application listen to Port 8000
+    app.listen(8000)
+
+    ## Return the application
+    return app
+
+if __name__ == "__main__":
+    ## Create application
+    app = MakeApp()
+
+    ## Run application forever
+    asyncio.get_event_loop().run_forever()
