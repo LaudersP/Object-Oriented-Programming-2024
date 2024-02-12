1fbb0b751a694eb5f862256b5d4323f952d141c4
Parker Lauders <parkerlauders04@gmail.com>
2024-02-12 10:20:30 -0500

    Basic code to update name, dob, and email

--- Forms/html/updateprofile.js
+++ Forms/html/updateprofile.js
@@ -1,10 +1,27 @@
 "use strict";
 
 function submit() {
-    let uname = document.getElementById("uname").value;
     let rname = document.getElementById("rname").value;
     let dob = document.getElementById("dob").value;
     let email = document.getElementById("email").value;
 
-    console.log("Info", uname, rname, dob, email);
+    let J = {
+        realName: rname,
+        birthDate: dob,
+        email: email
+    };
+
+    fetch("/profilev2",
+        {
+            method: "POST",
+            body: JSON.stringify(J)
+        }
+    ).then((resp) => {
+        resp.json().then((J) => {
+            console.log("Server said:", J);
+            document.location = document.location
+        });
+    }).catch((err) => {
+        console.log("Uh oh", err);
+    })
 }
--- Forms/src/ProfileV2.py
+++ Forms/src/ProfileV2.py
@@ -1,14 +1,46 @@
 import tornado.web
+import json
 
-username = "dave"
-name = "Dave N. Port"
-dateOfBirth = "May 4, 1999"
-email = "dave@dave.dave"
+accountDatabase = {
+    "alice": {
+        "real_name": "Alice Smith",
+        "DOB": "Jan. 1",
+        "email": "alice@example.com"
+    },
+    "bob": {
+        "real_name": "Bob Jones",
+        "DOB": "Dec. 31",
+        "email": "bob@bob.xyz"
+    },
+    "carol": {
+        "real_name": "Carol Ling",
+        "DOB": "Jul. 17",
+        "email": "carol@example.com"
+    },
+    "dave": {
+        "real_name": "Dave N. Port",
+        "DOB": "Mar. 14",
+        "email": "dave@dave.dave"
+    }
+}
 
 class Handler(tornado.web.RequestHandler):
     def get(self):
         self.render('../html/ProfileV2.html',
-                    userName = username,
-                    name = name,
-                    dateOfBirth = dateOfBirth,
-                    email = email)
+                    userName = "dave",
+                    name = accountDatabase["dave"]['real_name'],
+                    dateOfBirth = accountDatabase["dave"]['DOB'],
+                    email = accountDatabase["dave"]['email'])
+        
+    def post(self):
+        J = json.loads(self.request.body)
+        accountDatabase["dave"]['real_name'] = J["realName"]
+        accountDatabase["dave"]['DOB'] = J["birthDate"]
+        accountDatabase["dave"]['email'] = J["email"]
+
+        print("Name: ", accountDatabase["dave"]['real_name'])
+        print("DOB: ", accountDatabase["dave"]['DOB'])
+        print("email: ", accountDatabase["dave"]['email'])
+
+        resp={"ok": True}
+        self.write(json.dumps(resp))
